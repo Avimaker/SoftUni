eb8b77ee276b85f2d486d998d19c4de9a337a560
Avimaker <Avimaker@users.noreply.github.com>
2023-09-22 21:02:01 +0300

    update to solution 5

---
+++ Fund-Sep-2023/09.DataTypesAndVariables-Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+5
+A
+b
+C
+d
+E
+*/
+
+//Input
+int numOfInputLines = int.Parse(Console.ReadLine());
+int totalSum = 0;
+
+//Operations
+for (int i = 1; i <= numOfInputLines; i++)
+{
+    char letter = char.Parse(Console.ReadLine());
+
+    totalSum += letter;
+}
+
+
+//Output
+Console.WriteLine($"The sum equals: {totalSum}");        
---
+++ Fund-Sep-2023/09.DataTypesAndVariables-Exercise/05.PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,9 @@
+﻿// Input
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for (int i = start; i <= end; i++)
+{
+    Console.Write((char)i);
+    Console.Write(" ");
+}
