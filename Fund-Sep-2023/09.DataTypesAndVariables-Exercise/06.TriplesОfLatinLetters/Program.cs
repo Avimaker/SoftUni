a11e3d60cf5f4f266643820d76e4f965bcd708f2
Avimaker <Avimaker@users.noreply.github.com>
2023-09-22 22:44:09 +0300

    triple of letters - done

--- Fund-Sep-2023/09.DataTypesAndVariables-Exercise/06.TriplesОfLatinLetters/Program.cs
+++ Fund-Sep-2023/09.DataTypesAndVariables-Exercise/06.TriplesОfLatinLetters/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿// Input
+
+int number = int.Parse(Console.ReadLine());
+
+
+for (int i = 'a'; i < 'a' + number; i++)
+{
+    for (int k = 'a'; k < 'a' + number; k++)
+    {
+        for (int n = 'a'; n < 'a' + number; n++)
+        {
+            Console.WriteLine($"{(char)i}{(char)k}{(char)n}");
+
+
+        }
+
+    }
+
+
+}
+
+//for (char first = 'a'; first < 'a' + number; first++)
+//{
+//    for (char second = 'a'; second < 'a' + number; second++)
+//    {
+//        for (char third = 'a'; third < 'a' + number; third++)
+//        {
+//            Console.WriteLine($"{first}{second}{third}");
+//        }
+//    }
+//}
