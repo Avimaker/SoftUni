ce8a3df3333cbcf95a428ff9a17317a936c4315c
Avimaker <Avimaker@users.noreply.github.com>
2023-09-22 19:33:01 +0300

    solution 3 - done

---
+++ Fund-Sep-2023/09.DataTypesAndVariables-Exercise/03.Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿int persons = int.Parse(Console.ReadLine());
+int capacity = int.Parse(Console.ReadLine());
+
+int course = persons / capacity;
+int addCourse = persons % capacity;
+
+
+if (addCourse != 0 && addCourse < capacity)
+{
+    course++;
+
+}
+
+Console.WriteLine(course);
