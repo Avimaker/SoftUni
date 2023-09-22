290b1dac84d00f0482b46af25ac2d6d3867b20d0
Avimaker <Avimaker@users.noreply.github.com>
2023-09-22 18:49:52 +0300

    try to understand what exactly i synchronise

---
+++ Fund-Sep-2023/09.DataTypesAndVariables-Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstN = int.Parse(Console.ReadLine());
+int secondN = int.Parse(Console.ReadLine());
+int thirdN = int.Parse(Console.ReadLine());
+int fourthN = int.Parse(Console.ReadLine());
+
+int opeationOne = firstN + secondN;
+int operationTwo = opeationOne / thirdN;
+int operationThree = operationTwo * fourthN;
+
+Console.WriteLine(operationThree);
