8c5d2ac035e01bd7e18f09ba029ac14b54839327
Avimaker <Avimaker@users.noreply.github.com>
2023-10-01 00:48:08 +0300

    finished task 6

--- Fund-Sep-2023/12.Arrays-Exercise/12.Arrays-Exercise/06.EqualSum/Program.cs
+++ Fund-Sep-2023/12.Arrays-Exercise/12.Arrays-Exercise/06.EqualSum/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int sumeLeft = 0;
+    int sumeRight = 0;
+
+    // Calculate the sum of elements to the left
+    for (int j = 0; j < i; j++)
+    {
+        sumeLeft += numbers[j];
+    }
+
+    // Calculate the sum of elements to the right
+    for (int k = i + 1; k < numbers.Length; k++)
+    {
+        sumeRight += numbers[k];
+    }
+
+    if (sumeLeft == sumeRight)
+    {
+        Console.WriteLine(i);
+        return; // Stop searching as we found the index
+    }
+}
+
+Console.WriteLine("no"); // No such index exists
