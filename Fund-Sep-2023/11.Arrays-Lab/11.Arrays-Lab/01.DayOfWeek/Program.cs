e1ce6d50bb3b8c0b04714565695db098fbac47bc
Avimaker <Avimaker@users.noreply.github.com>
2023-10-04 18:22:59 +0300

    last updat before start methods lab

---
+++ Fund-Sep-2023/11.Arrays-Lab/11.Arrays-Lab/01.DayOfWeek/Program.cs
@@ -0,0 +1,11 @@
+﻿int dayNumber = int.Parse(Console.ReadLine());
+string[] daysOfTheWeek = new string[7] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+
+if (dayNumber >= 1 && dayNumber <= 7)
+{
+    Console.WriteLine(daysOfTheWeek[dayNumber-1]);
+    return;
+}
+
+Console.WriteLine("Invalid day!");
---
+++ Fund-Sep-2023/11.Arrays-Lab/11.Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[] masiv = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+int currentNumber = int.Parse(Console.ReadLine());
+    masiv[i] = currentNumber;
+}
+
+for (int i = masiv.Length - 1; i >= 0; i--)
+{
+    Console.Write(masiv[i] + " ");
+}
+
