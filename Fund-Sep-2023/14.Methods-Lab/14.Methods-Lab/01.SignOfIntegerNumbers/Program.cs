03e42f0e4db9d4203971d7d4c05d381c652d1538
Avimaker <Avimaker@users.noreply.github.com>
2023-10-04 20:09:11 +0300

    Methods - Lab - task 1 finished

--- Fund-Sep-2023/14.Methods-Lab/14.Methods-Lab/01.SignOfIntegerNumbers/Program.cs
+++ Fund-Sep-2023/14.Methods-Lab/14.Methods-Lab/01.SignOfIntegerNumbers/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _01.SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
 
