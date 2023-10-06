4e61fe8ca944e7930379421aacff5050e45febca
Avimaker <Avimaker@users.noreply.github.com>
2023-10-06 23:18:51 +0300

    task 9 finished

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/08.FactorialDivision/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/08.FactorialDivision/Program.cs
@@ -14,30 +14,19 @@ namespace _08.FactorialDivision
             int firsNum = int.Parse(Console.ReadLine());
             int secondNum = int.Parse(Console.ReadLine());
                                    
-            Console.WriteLine($"{(firstFact(firsNum) / secondFact(secondNum)):F2}");
+            Console.WriteLine($"{(Factorial(firsNum) / Factorial(secondNum)):F2}");
 
         }
 
-        static double secondFact(int secondNum)
+        static double Factorial(int number)
         {
-            double factorialTwo = 1;
-            for (int i = 1; i <= secondNum; i++)
+            double factorial = 1;
+            for (int i = 1; i <= number; i++)
             {
-                factorialTwo *= i;
+                factorial *= i;
             }
 
-            return factorialTwo;
-        }
-
-        static double firstFact(int firsNum)
-        {
-            double factorialOne = 1;
-            for (int i = 1; i <= firsNum; i++)
-            {
-                factorialOne *= i;
-            }
-
-            return factorialOne;
+            return factorial;
         }
     }
 }
--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/09.PalindromeIntegers/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -1,4 +1,54 @@
-﻿using System;
+﻿/*
+123
+323
+421
+121
+END
+*/
+
+
+//using System;
+
+//namespace _09.PalindromeIntegers
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input = "";
+
+//            while ((input = Console.ReadLine()) != "END")
+//            {
+//                bool isNumberPalindrome = IsPalindrome(input);
+//                Console.WriteLine(isNumberPalindrome);
+//            }
+
+//            static bool IsPalindrome(string symbols)
+//            {
+//                int left = 0;
+//                int right = symbols.Length - 1;
+
+//                while (left < right)
+//                {
+//                    if (symbols[left] != symbols[right])
+//                    {
+//                        return false;
+//                    }
+
+//                    left++;
+//                    right--;
+
+//                }
+
+//                return true;
+//            }
+//        }
+//    }
+//}
+
+
+
+using System;
 
 namespace _09.PalindromeIntegers
 {
@@ -6,8 +56,27 @@ namespace _09.PalindromeIntegers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                bool isNumberPalindrome = IsPalindrome(input);
+                Console.WriteLine(isNumberPalindrome);
+            }
+
+            static bool IsPalindrome(string symbols)
+            {
+                string first = symbols.Substring(0, symbols.Length / 2);
+                char[] arr = symbols.ToCharArray();
+
+                Array.Reverse(arr);
+
+                string temp = new string(arr);
+                string second = temp.Substring(0, symbols.Length / 2);
+
+                return first.Equals(second);
+
+            }
         }
     }
 }
-
