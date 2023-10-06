a04384ff1a9b840708fa9458417418699538f11a
Avimaker <Avimaker@users.noreply.github.com>
2023-10-06 20:21:57 +0300

    Task 7 Finished

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/07.NxN atrix/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/07.NxN atrix/Program.cs
@@ -6,7 +6,27 @@ namespace _07.NxN_atrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            int[] arr = new int[number];
+
+            FillArray(number, arr);
+            PrintMatrix(arr);
+        }
+
+        static void PrintMatrix(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+        }
+
+        static void FillArray(int number, int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = number;
+            }
         }
     }
 }
