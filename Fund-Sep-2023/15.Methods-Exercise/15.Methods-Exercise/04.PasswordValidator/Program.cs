d327dae9e5b771c97026f23c35c1266b33c99877
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 15:18:10 +0300

    PasswordValidator - Finished
    I am Proud of myself :)
    Find a way to fix the bool reseting and achieve 100% in judge

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/04.PasswordValidator/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/04.PasswordValidator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _04.PasswordValidator
 {
@@ -6,8 +7,81 @@ namespace _04.PasswordValidator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isTrue = true;
+            string passInput = Console.ReadLine();
+
+            int boolCounter = 0;
+
+            isTrue = CharactersLength(passInput);
+            if (!isTrue) { boolCounter++;}
+            isTrue = CheckForSymbols(passInput);
+            if (!isTrue) { boolCounter++; }
+            isTrue = DigitCounter(passInput);
+            if (!isTrue) { boolCounter++; }
+
+
+            if (isTrue && boolCounter == 0)
+            {
+                Console.WriteLine("Password is valid");
+            }
         }
+
+        static bool CharactersLength(string passInput)
+        {
+            bool isTrue;
+            if (passInput.Length < 6 || passInput.Length > 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+                isTrue = false;
+                return isTrue;
+            }
+            isTrue = true;
+            return isTrue;
+        }
+
+        static bool CheckForSymbols(string passInput)
+        {
+            bool isTrue;
+            string forbiden = "^[a-zA-Z0-9]+$";
+
+            if (!Regex.IsMatch(passInput, forbiden))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+                isTrue = false;
+                return isTrue;
+
+            }
+
+            isTrue = true;
+            return isTrue;
+        }
+
+        static bool DigitCounter(string passInput)
+        {
+            bool isTrue;
+
+            int digitCounter = 0;
+            for (int i = 0; i < passInput.Length; i++)
+            {
+                char check = passInput[i];
+                if (check == '0' || check == '1' || check == '2'
+                    || check == '3' || check == '4' || check == '5'
+                    || check == '6' || check == '7' || check == '8'
+                    || check == '9')
+                {
+                    digitCounter++;
+                }
+            }
+            if (digitCounter < 2)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+                isTrue = false;
+                return isTrue;
+            }
+            isTrue = true;
+            return isTrue;
+        }
+
     }
 }
 
