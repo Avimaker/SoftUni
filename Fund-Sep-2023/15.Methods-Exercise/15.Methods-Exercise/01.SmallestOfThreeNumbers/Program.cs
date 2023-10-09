4fe0a54e6f63b0f73098672e31739ffb2da0a34f
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 09:54:02 +0300

    Smallest of 3 finished

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -6,8 +6,27 @@ namespace _01.SmallestOfThreeNumbers
     {
         static void Main(string[] args)
         {
-
             int n = int.Parse(Console.ReadLine());
+            int smallestNumber = n;
+
+            SmallestNumCounting(ref n, ref smallestNumber);
+
+            Console.WriteLine(smallestNumber);
+
+        }
+
+        static void SmallestNumCounting(ref int n, ref int smallestNumber)
+        {
+            for (int i = 1; i < 3; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n < smallestNumber)
+
+                {
+                    smallestNumber = n;
+                }
+
+            }
         }
     }
 }
