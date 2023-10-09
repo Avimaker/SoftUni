4575e02085521660a242545d28ebbf2342e675bd
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 15:46:32 +0300

    AddAndSubtract - finished
    this was easy one :)

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/05.AddAndSubtract/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/05.AddAndSubtract/Program.cs
@@ -6,7 +6,25 @@ namespace _05.AddAndSubtract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int digitOne = int.Parse(Console.ReadLine());
+            int digitTwo = int.Parse(Console.ReadLine());
+            int digitThree = int.Parse(Console.ReadLine());
+
+            int firstTwoSume = FirstSume(digitOne, digitTwo);
+            int result = TotalSume(firstTwoSume, digitThree);
+
+            Console.WriteLine(result);
+
+        }
+
+        static int FirstSume(int digitOne, int digitTwo)
+        {
+            return digitOne + digitTwo;
+        }
+
+        static int TotalSume(int firstTwoSume, int digitThree)
+        {
+            return firstTwoSume - digitThree;
         }
     }
 }
