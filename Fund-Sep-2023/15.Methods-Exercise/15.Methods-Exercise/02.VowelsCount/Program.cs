34fb2a2a841cb67585c243a42e663c7161e3f597
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 10:11:36 +0300

    VowelsCount finished

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/02.VowelsCount/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/02.VowelsCount/Program.cs
@@ -6,8 +6,47 @@ namespace _02.VowelsCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            int vowelsCount = 0;
+
+            vowelsCount = VovelCheck(input, vowelsCount);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        private static int VovelCheck(string input, int vowelsCount)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                char check = input[i];
+
+                if (check == 'a' || check == 'A')
+                {
+                    vowelsCount++;
+                }
+                else if (check == 'e' || check == 'E')
+                {
+                    vowelsCount++;
+                }
+                else if (check == 'i' || check == 'I')
+                {
+                    vowelsCount++;
+                }
+                else if (check == 'o' || check == 'O')
+                {
+                    vowelsCount++;
+                }
+                else if (check == 'u' || check == 'U')
+                {
+                    vowelsCount++;
+                }
+
+            }
+
+            return vowelsCount;
         }
     }
 }
+
 
