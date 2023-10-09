297d4184e6d10cbce650122c084440c63fca1ac2
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 16:02:16 +0300

    MiddleCharacters - finishd
    Also easy one

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/06.MiddleCharacters/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -6,7 +6,24 @@ namespace _06.MiddleCharacters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int length = input.Length;
+            EvenMiddeSymbolPrint(input, length);
+
+        }
+
+        private static void EvenMiddeSymbolPrint(string input, int length)
+        {
+            if (length % 2 == 0)
+            {
+                int middle = length / 2;
+                Console.WriteLine($"{input[middle-1]}{input[(middle)]}");
+            }
+            else if (length % 2 != 0)
+            {
+                int middle = length / 2;
+                Console.WriteLine($"{input[(middle)]}");
+            }
         }
     }
 }
