a9d4cd280bf8c0d1a6f04749e0c15b0292da392a
Avimaker <Avimaker@users.noreply.github.com>
2023-10-09 19:01:04 +0300

    topNumber finished
    Very shitty task :(

--- Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/10.TopNumber/Program.cs
+++ Fund-Sep-2023/15.Methods-Exercise/15.Methods-Exercise/10.TopNumber/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 
-namespace _10.TopNumber
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int range = int.Parse(Console.ReadLine());
+
+        for (int number = 1; number <= range; number++)
         {
-            Console.WriteLine("Hello World!");
+            if (IsTopNumber(number))
+            {
+                Console.WriteLine(number);
+            }
         }
     }
-}
 
+    static bool IsTopNumber(int number)
+    {
+        return IsDivisibleBy8(number) && CheckForOddDigit(number);
+    }
+
+    static bool IsDivisibleBy8(int number)
+    {
+        int currentSumme = 0;
+        int tempNumber = number;
+
+        while (tempNumber > 0)
+        {
+            int digit = tempNumber % 10;
+            currentSumme += digit;
+            tempNumber /= 10;
+        }
+
+        return currentSumme % 8 == 0;
+    }
+
+    static bool CheckForOddDigit(int number)
+    {
+        while (number > 0)
+        {
+            int digit = number % 10;
+            if (digit % 2 != 0)
+            {
+                return true;
+            }
+            number /= 10;
+        }
+
+        return false;
+    }
+}
