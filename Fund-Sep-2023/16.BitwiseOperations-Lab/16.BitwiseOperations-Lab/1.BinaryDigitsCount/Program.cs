f22ab3dc871ad430380b871fe07eb8208ab9a50a
Avimaker <Avimaker@users.noreply.github.com>
2023-10-11 21:56:57 +0300

    lists - lab

---
+++ Fund-Sep-2023/16.BitwiseOperations-Lab/16.BitwiseOperations-Lab/1.BinaryDigitsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+int binary = int.Parse(Console.ReadLine());
+
+int zeroesCounter = 0;
+int onesCounter = 0;
+
+while (number > 0)
+{
+
+    int tempNumber = number % 2;
+    number /= 2;
+
+    if (tempNumber == 1)
+    {
+        onesCounter++;
+    }
+    else
+    {
+        zeroesCounter++;
+    }
+
+   
+}
+
+if (binary == 0)
+{
+    Console.WriteLine(zeroesCounter);
+}
+else
+{
+    Console.WriteLine(onesCounter);
+}
---
+++ Fund-Sep-2023/17.List-Lab/17.List-Lab/02.GaussTrick/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.GaussTrick;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < numbers.Count / 2 ; i++)
+        {
+            int first = numbers[i];
+            int last = numbers[numbers.Count - i - 1];
+            result.Add(first + last);
+        }
+
+        if (numbers.Count % 2 == 1)
+        {
+            int middleIndex = numbers.Count / 2;
+            result.Add(numbers[middleIndex]);
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
+
