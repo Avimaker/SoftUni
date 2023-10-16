4060c2513a150db07f6ab588a6bc47616cd6e11d
Avimaker <Avimaker@users.noreply.github.com>
2023-10-16 13:11:42 +0300

    last task finished - end :)

---
+++ Fund-Sep-2023/18.List-Execise/18.List-Execise/07.AppendArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+7 | 4  5|1 0| 2 5 |3
+ */
+
+namespace _07.AppendArrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] stringArray = Console.ReadLine()
+            .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+        List<string> symbols = ExtractSymbols(stringArray);
+
+        Console.WriteLine(string.Join(" ", symbols));
+    }
+
+    private static List<string> ExtractSymbols(string[] stringArray)
+    {
+
+        List<string> result = new List<string>();
+
+        for (int i = stringArray.Length - 1; i >= 0; i--)
+        {
+            string sequence = stringArray[i];
+            string[] array = sequence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            result.AddRange(array);
+        }
+        return result;
+    }
+}
+
