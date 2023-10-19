ac478e331b5beafb267722bc7065f8c33e6f3572
Avimaker <Avimaker@users.noreply.github.com>
2023-10-19 17:51:25 +0300

    04.ComputerStore; solved by my self w/o any additional help :)

--- Fund-Sep-2023/19.ExamPreparation/19.ExamPreparation/04.ComputerStore/Program.cs
+++ Fund-Sep-2023/19.ExamPreparation/19.ExamPreparation/04.ComputerStore/Program.cs
@@ -1,10 +1,90 @@
-﻿namespace _04.ComputerStore;
+﻿/*
+1050
+200
+450
+2
+18.50 
+16.86 
+special
+ 
+1023 
+15
+-20
+-5.50
+450
+20 
+17.66 
+19.30
+regular
+
+regular
+ 
+ */
+
+
+namespace _04.ComputerStore;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double currentPrice = 0;
+        double tax = 0;
+        double totalPrice = 0;
+
+        string inputs = default;
+        string custumer = default;
+        bool isTrue = true;
+        while (isTrue)
+        {
+            inputs = Console.ReadLine();
+
+            if (inputs != "special" && inputs != "regular")
+            {
+                double priceCheck = double.Parse(inputs);
+
+                if (priceCheck < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    currentPrice += priceCheck;
+                }
+            }
+            else
+            {
+                custumer = inputs;
+                isTrue = false;
+            }
+
+        }
+
+        if (custumer == "regular")
+        {
+            tax = currentPrice * 0.20;
+            totalPrice = currentPrice + tax;
+        }
+        else if (custumer == "special")
+        {
+            tax = currentPrice * 0.20;
+            totalPrice = currentPrice + tax;
+            totalPrice *= 0.90;
+        }
+
+        if (totalPrice == 0)
+        {
+            Console.WriteLine("Invalid order!");
+            return;
+        }
+
+        Console.WriteLine("Congratulations you've just bought a new computer!");
+        Console.WriteLine($"Price without taxes: {currentPrice:f2}$");
+        Console.WriteLine($"Taxes: {tax:f2}$");
+        Console.WriteLine("-----------");
+        Console.WriteLine($"Total price: {totalPrice:f2}$");
+
+
     }
 }
 
