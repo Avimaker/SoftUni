4d544cf32f70ef89dd282d082afc8258ea158182
Avimaker <Avimaker@users.noreply.github.com>
2023-11-02 22:53:49 +0200

    all problems solved

---
+++ Fund-Sep-2023/25.AssociativeArrays/25.AssociativeArrays/04.WordFilter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.WordFilter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ////Var 1
+        //List<string> words = Console.ReadLine()
+        //    .Split()
+        //    .Where(x => x.Length % 2 == 0)
+        //    .ToList();
+
+        //Console.WriteLine(string.Join(Environment.NewLine, words));
+
+        // Var 2
+
+        Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine()
+            .Split()
+            .Where(x => x.Length % 2 == 0)
+            .ToList()));
+
+
+    }
+}
+
+
