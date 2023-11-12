b1b73deb09e2d33fa0ca83cbac5e87fd7e9e0c04
Avimaker <Avimaker@users.noreply.github.com>
2023-11-12 20:57:10 +0200

    text processing problem homework

---
+++ Fund-Sep-2023/29.TextProcessing-Exercise/29.TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+sh, too_long_username, !lleg@l ch@rs, jeffbutt
+*/
+
+namespace _01.ValidUsernames;
+
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] userNames = Console.ReadLine()
+            .Split(", ");
+
+        foreach (string userName in userNames)
+        {
+
+            if (userName.Length < 3 || userName.Length > 16)
+            {
+                continue;
+            }
+
+            if (IsValid(userName))
+            {
+                Console.WriteLine(userName);
+            }
+
+        }
+
+    }
+
+    private static bool IsValid(string userName)
+    {
+
+        return userName.All(ch => char.IsLetterOrDigit(ch) ||
+                            ch == '-' || ch == '_');
+    }
+}
+
