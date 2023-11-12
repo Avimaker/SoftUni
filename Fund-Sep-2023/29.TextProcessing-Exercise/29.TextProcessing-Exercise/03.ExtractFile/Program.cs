ce378f373d65c76500d5bff1349ca67194474284
Avimaker <Avimaker@users.noreply.github.com>
2023-11-12 21:50:44 +0200

    problem 3 solved

--- Fund-Sep-2023/29.TextProcessing-Exercise/29.TextProcessing-Exercise/03.ExtractFile/Program.cs
+++ Fund-Sep-2023/29.TextProcessing-Exercise/29.TextProcessing-Exercise/03.ExtractFile/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace _03.ExtractFile;
+﻿/*
+C:\Internal\training-internal\Template.pptx
+*/
+
+using System;
+using System.Linq;
+using System.IO;
+
+namespace _03.ExtractFile;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var filePath = Console.ReadLine();
+
+        string fileName = string.Empty;
+        string fileExtension = string.Empty;
+
+        int lastSeparatorIndex = filePath.LastIndexOf('\\');
+        int extensionIndex = filePath.LastIndexOf('.');
+
+
+        fileName = filePath.Substring(lastSeparatorIndex + 1, extensionIndex - lastSeparatorIndex );
+
+
+        Console.WriteLine($"File name: {fileName}");
+        Console.WriteLine($"File extension: {Path.GetExtension(filePath).Replace(".", "")}");
+
     }
+
+
 }
 
