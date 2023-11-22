7ef7fd3da93832d04cc64d334a53378d42e56ea4
Avimaker <Avimaker@users.noreply.github.com>
2023-11-22 19:56:48 +0200

    problem one solved

--- Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/01.SecretChat/Program.cs
+++ Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/01.SecretChat/Program.cs
@@ -4,7 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string secretMsg = Console.ReadLine();
+
+        string line;
+
+        while ((line = Console.ReadLine()) != "Reveal")
+        {
+            string[] tokens = line.Split(":|:");
+            string command = tokens[0];
+
+            if (command == "InsertSpace")
+            {
+                int index = int.Parse(tokens[1]);
+                secretMsg = secretMsg.Insert(index, " ");
+            }
+            else if (command == "ChangeAll")
+            {
+                string substring = tokens[1];
+                string replacement = tokens[2];
+                secretMsg = secretMsg.Replace(substring, replacement);
+            }
+            else //Reverse
+            {
+                string substring = tokens[1];
+                int substringIndex = secretMsg.IndexOf(substring);
+                if (substringIndex == -1)
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                secretMsg = secretMsg.Remove(substringIndex, substring.Length);
+                string reversedSubstring = new string(substring.Reverse().ToArray());
+                secretMsg += reversedSubstring;
+            }
+            Console.WriteLine(secretMsg);
+
+        }
+        Console.WriteLine($"You have a new text message: {secretMsg}");
     }
 }
 
