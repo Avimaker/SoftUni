478b3682689fb19df256acd9ac27486dec2fb978
Avimaker <Avimaker@users.noreply.github.com>
2023-11-26 21:44:20 +0200

    list of heroes solved

--- Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/05.EmojiDetector/Program.cs
+++ Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/05.EmojiDetector/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace _05.EmojiDetector;
+﻿/*
+In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**
+
+5, 4, 3, 2, 1, go! The 1-th consecutive banana-eating contest has begun! ::Joy:: **Banana** ::Wink:: **Vali** ::valid_emoji::
+
+It is a long established fact that 1 a reader will be distracted by 9 the readable content of a page when looking at its layout. The point of using ::LoremIpsum:: is that it has a more-or-less normal 3 distribution of 8 letters, as opposed to using 'Content here, content 99 here', making it look like readable **English**.
+ 
+*/
+
+using System.Text.RegularExpressions;
+
+namespace _05.EmojiDetector;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        string emojiPattern = @"(\*{2}|\:{2})(?<Emoji>[A-Z][a-z]{2,})\1";
+        string coolTresholdPattern = @"\d";
+
+        List<string> coolEmojies = new List<string>();
+
+        string input = Console.ReadLine();
+
+        ulong coolThreshold = 1;
+
+        foreach (Match match in Regex.Matches(input, coolTresholdPattern))
+        {
+            coolThreshold *= ulong.Parse(match.Value);
+        }
+
+        MatchCollection matches = Regex.Matches(input, emojiPattern);
+        foreach (Match match in matches)
+        {
+            string emojiString = match.Groups["Emoji"].Value;
+
+            ulong totalEmojiSum = 0;
+            foreach (char character in emojiString)
+            {
+                totalEmojiSum += character;
+            }
+
+            if (totalEmojiSum >= coolThreshold)
+            {
+                coolEmojies.Add(match.Value);
+            }
+        }
+
+        Console.WriteLine($"Cool threshold: {coolThreshold}");
+        Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+        coolEmojies.ForEach(emoji => Console.WriteLine(emoji));
+
     }
 }
-
--- Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/05.HeroesOfCodeAndLogicVII/Program.cs
+++ Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/05.HeroesOfCodeAndLogicVII/Program.cs
@@ -22,16 +22,134 @@ End
 
 */
 
-using System.Text.RegularExpressions;
-
 namespace _05.HeroesOfCodeAndLogicVII;
+
+class Hero
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+    public int MP { get; set; }
+
+    public Hero(string name, int hP, int mP)
+    {
+        Name = name;
+        HP = hP;
+        MP = mP;
+    }
+}
+
 
 class Program
 {
-    static void Main(string[] args)
-    {
+    static List<Hero> heroes = new List<Hero>();
 
-       
+    static void Main()
+    {
+        int heroesCount = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < heroesCount; i++)
+        {
+            string[] heroInfo = Console.ReadLine().Split();
+            string name = heroInfo[0];
+            int hp = Math.Min(int.Parse(heroInfo[1]), 100);
+            int mp = Math.Min(int.Parse(heroInfo[2]), 200);
+
+            Hero hero = new Hero(name, hp, mp);
+            heroes.Add(hero);
+        }
+
+        string command;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] arguments = command.Split(" - ");
+            string action = arguments[0];
+            string heroName = arguments[1];
+
+            if (action == "CastSpell")
+            {
+                int mpNeeded = int.Parse(arguments[2]);
+                string spellName = arguments[3];
+                CastSpell(heroName, mpNeeded, spellName);
+            }
+            else if (action == "TakeDamage")
+            {
+                int damage = int.Parse(arguments[2]);
+                string attacker = arguments[3];
+                TakeDamage(heroName, damage, attacker);
+            }
+            else if (action == "Recharge")
+            {
+                int amount = int.Parse(arguments[2]);
+                Recharge(heroName, amount);
+            }
+            else if (action == "Heal")
+            {
+                int amount = int.Parse(arguments[2]);
+                Heal(heroName, amount);
+            }
+        }
+
+        PrintAliveHeroes(heroes);
+    }
+
+    static void CastSpell(string heroName, int mpNeeded, string spellName)
+    {
+        Hero foundHero = heroes.FirstOrDefault(h => h.Name == heroName);
+
+        if (foundHero.MP >= mpNeeded)
+        {
+            foundHero.MP -= mpNeeded;
+            Console.WriteLine($"{foundHero.Name} has successfully cast {spellName} and now has {foundHero.MP} MP!");
+        }
+        else
+        {
+            Console.WriteLine($"{foundHero.Name} does not have enough MP to cast {spellName}!");
+        }
+    }
+
+    static void TakeDamage(string heroName, int damage, string attacker)
+    {
+        Hero foundHero = heroes.FirstOrDefault(h => h.Name == heroName);
+
+        foundHero.HP -= damage;
+        if (foundHero.HP > 0)
+        {
+            Console.WriteLine($"{foundHero.Name} was hit for {damage} HP by {attacker} and now has {foundHero.HP} HP left!");
+        }
+        else
+        {
+            Console.WriteLine($"{foundHero.Name} has been killed by {attacker}!");
+        }
+    }
+
+    static void Recharge(string heroName, int amount)
+    {
+        Hero foundHero = heroes.FirstOrDefault(h => h.Name == heroName);
+
+        int recovered = Math.Min(200 - foundHero.MP, amount);
+        foundHero.MP += recovered;
+        Console.WriteLine($"{foundHero.Name} recharged for {recovered} MP!");
+    }
+
+    static void Heal(string heroName, int amount)
+    {
+        Hero foundHero = heroes.FirstOrDefault(h => h.Name == heroName);
+
+        int recovered = Math.Min(100 - foundHero.HP, amount);
+        foundHero.HP += recovered;
+        Console.WriteLine($"{foundHero.Name} healed for {recovered} HP!");
+    }
+
+    static void PrintAliveHeroes(List<Hero> heroes)
+    {
+        foreach (var hero in heroes)
+        {
+            if (hero.HP > 0)
+            {
+                Console.WriteLine($"{hero.Name}\n  HP: {hero.HP}\n  MP: {hero.MP}");
+            }
+        }
     }
 }
 
