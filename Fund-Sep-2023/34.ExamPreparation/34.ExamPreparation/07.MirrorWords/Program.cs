2f3a72b28241c26bbb5ec75d71f4581b267f7a58
Avimaker <Avimaker@users.noreply.github.com>
2023-12-01 00:05:32 +0200

    Mirror Words - 88/100

--- Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/07.MirrorWords/Program.cs
+++ Fund-Sep-2023/34.ExamPreparation/34.ExamPreparation/07.MirrorWords/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace _07.MirrorWords;
+﻿/*
+@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r
+
+#po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@
+
+#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#
+
+pattern:
+(\@{1}|\#{1})(?<word>[A-Za-z]{3,})(\1)(\@{1}|\#{1})(?<mirror>[A-Za-z]{3,})(\1)
+ 
+*/
+
+
+using System.Text.RegularExpressions;
+
+namespace _07.MirrorWords;
+
+class Pair
+{
+    public Pair(string word, string mirror)
+    {
+        Word = word;
+        Mirror = mirror;
+    }
+
+    public string Word { get; set; }
+    public string Mirror { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Word} <=> {Mirror}";
+    }
+}
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        string input = Console.ReadLine();
+        string pattern = @"(\@{1}|\#{1})(?<word>[A-Za-z]{3,})(\1)(\@{1}|\#{1})(?<mirror>[A-Za-z]{3,})(\1)";
+
+        List<Pair> binded = new List<Pair>();
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        foreach (Match match in matches)
+        {
+            string word = match.Groups["word"].Value;
+            string mirror = match.Groups["mirror"].Value;
+
+            char[] reversedMirrorChar = mirror.ToCharArray();
+            Array.Reverse(reversedMirrorChar);
+            string reversedMirror = new string(reversedMirrorChar);
+
+            if (word == reversedMirror)
+            {
+                Pair currentPair = new Pair(word, mirror);
+                binded.Add(currentPair);
+            }
+        }
+
+
+        if (matches.Count == 0)
+        {
+            Console.WriteLine("No word pairs found!");
+        }
+        else
+        {
+            Console.WriteLine($"{matches.Count} word pairs found!");
+        }
+
+        if (binded.Count > 0)
+        {
+            Console.WriteLine("The mirror words are:");
+            Console.WriteLine(string.Join(", ", binded));
+        }
+        else
+        {
+            Console.WriteLine("No mirror words!");
+        }
     }
 }
 
