3fde459532ebef9f2db1fd599d2080753ecad2c6
Avimaker <Avimaker@users.noreply.github.com>
2024-01-15 00:51:07 +0200

    Problem 5 - home work almost solved

--- Advanced-Jan-2024/05.MultidimensionalArrays/5.SquareWithMaximumSum/Program.cs
+++ Advanced-Jan-2024/05.MultidimensionalArrays/5.SquareWithMaximumSum/Program.cs
@@ -1,8 +1,14 @@
 ﻿/*
 3, 6
+2, 2
 7, 1, 3, 3, 2, 1
 1, 3, 9, 8, 5, 6
 4, 6, 7, 9, 1, 0
+
+9 8
+7 9
+33
+
 */
 
 namespace _5.SquareWithMaximumSum;
@@ -19,6 +25,21 @@ class Program
         int rows = input[0];
         int cols = input[1];
 
+        //Console.WriteLine("Please input col and row for squear separated by comma and space");
+        int[] squareInput = Console.ReadLine()
+        .Split(", ")
+        .Select(int.Parse)
+        .ToArray();
+
+        int sqRows = squareInput[0];
+        int sqCols = squareInput[1];
+
+        if (sqRows >= rows || sqCols >= cols)
+        {
+            Console.WriteLine("Invalid square input!");
+            return;
+        }
+
         int[,] matrix = ReadMatrix(rows, cols, ", ");
 
         int maxSquareSum = matrix[0, 0];
@@ -27,10 +48,17 @@ class Program
 
         for (int row = 0; row < rows - 1; row++)
         {
-            for (int col = 0; col < cols - 1; col++)
+            for (int col = 0; col < cols - sqCols; col++)
             {
-                int squareSum = matrix[row, col] + matrix[row, col + 1]
-                    + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                int squareSum = 0;
+
+                for (int srow = 0; srow < sqRows; srow++)
+                {
+                    for (int scol = 0; scol < sqCols; scol++)
+                    {
+                        squareSum += matrix[row + srow, col + scol];
+                    }
+                }
 
                 if (squareSum > maxSquareSum)
                 {
@@ -40,23 +68,17 @@ class Program
                 }
             }
         }
-         
 
-        Console.WriteLine($"{matrix[maxSquareRow, maxSquareCol]} {matrix[maxSquareRow, maxSquareCol + 1]}");
-        Console.WriteLine($"{matrix[maxSquareRow + 1, maxSquareCol]} {matrix[maxSquareRow + 1, maxSquareCol + 1]}");
+        for (int r = 0 ; r < sqRows; r++)
+        {
+            for (int c = 0; c < sqCols; c++)
+            {
+                Console.Write($"{matrix[maxSquareRow + r, maxSquareCol + c]} ");
+            }
+            Console.WriteLine();
+        }
+
         Console.WriteLine(maxSquareSum);
-
-
-
-        //Print Matrix
-        //for (int row = 0; row < matrix.GetLength(0); row++)
-        //{
-        //    for (int col = 0; col < matrix.GetLength(1); col++)
-        //    {
-        //        Console.Write($"{matrix[row, col]} ");
-        //    }
-        //    Console.WriteLine();
-        //}
 
     }
 
