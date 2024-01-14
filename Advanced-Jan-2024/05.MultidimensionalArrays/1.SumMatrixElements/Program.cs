fc4f36a90b38d158d6e83605e1913b4feab0fead
Avimaker <Avimaker@users.noreply.github.com>
2024-01-14 20:21:31 +0200

    Problem 2 solved

--- Advanced-Jan-2024/05.MultidimensionalArrays/1.SumMatrixElements/Program.cs
+++ Advanced-Jan-2024/05.MultidimensionalArrays/1.SumMatrixElements/Program.cs
@@ -40,16 +40,6 @@ class Program
         Console.WriteLine(sum);
 
 
-        //Print
-        for (int row = 0; row < matrix.GetLength(0); row++)
-        {
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            {
-                Console.Write($"{matrix[row, col]} ");
-            }
-            Console.WriteLine();
-        }
-
     }
 
 }
---
+++ Advanced-Jan-2024/05.MultidimensionalArrays/2.SumMatrixColumns/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+3, 6
+7 1 3 3 2 1
+1 3 9 8 5 6
+4 6 7 9 1 0
+*/
+
+namespace _2.SumMatrixColumns;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        int rows = input[0];
+        int cols = input[1];
+
+        int colSum = 0;
+
+        int[,] matrix = ReadMatrix(rows, cols, " ");
+
+        //sum
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                colSum += matrix[row, col];
+            }
+            Console.WriteLine(colSum);
+            colSum = 0;
+        }
+
+    }
+
+    
+    private static int[,] ReadMatrix(int rows, int cols, string separator)
+    {
+        int[,] matrix = new int[rows, cols];
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            int[] rowArray = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = rowArray[col];
+            }
+        }
+        return matrix;
+    }
+
+}
+
+
