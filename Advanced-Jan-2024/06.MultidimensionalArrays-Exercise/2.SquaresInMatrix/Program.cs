52ec62fcd38113d6e08ab3c2763fa1e5b037f56b
Avimaker <Avimaker@users.noreply.github.com>
2024-01-15 19:36:53 +0200

    Problem 2 solved

--- Advanced-Jan-2024/06.MultidimensionalArrays-Exercise/2.SquaresInMatrix/Program.cs
+++ Advanced-Jan-2024/06.MultidimensionalArrays-Exercise/2.SquaresInMatrix/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace _2.SquaresInMatrix;
+﻿/*
+3 4
+A B B D
+E B B B
+I J B B
+
+*/
+
+namespace _2.SquaresInMatrix;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int rows = size[0];
+        int cols = size[1];
+        int squaresFound = 0;
+        string[] previousRow = null;
+
+        for (int row = 0; row < rows; row++)
+        {
+            string[] currentRow = Console.ReadLine().Split();
+
+            for (int col = 0; col < cols - 1; col++)
+            {
+                if (row > 0)
+                {
+                    if (currentRow[col] == previousRow[col] &&
+                        currentRow[col] == previousRow[col + 1] &&
+                        currentRow[col + 1] == previousRow[col])
+                    {
+                        squaresFound++;
+                    }
+                }
+            }
+            previousRow = currentRow;
+        }
+        Console.WriteLine(squaresFound);
     }
 }
 
---
+++ Advanced-Jan-2024/06.MultidimensionalArrays-Exercise/3.MaximalSum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3.MaximalSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
