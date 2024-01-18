bfac5f77ae2184afc897f0755458f4dda1bfb092
Avimaker <Avimaker@users.noreply.github.com>
2024-01-18 23:12:55 +0200

    SetsOfElements - solved

---
+++ Advanced-Jan-2024/08.SetsAndDictionariesAdvanced-Exercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+4 3
+1
+3
+5
+7
+3
+4
+5
+*/
+
+namespace _02.SetsOfElements;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] sets = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int n = sets[0];
+        int m = sets[1];
+
+        HashSet<int> first = new HashSet<int>();
+        HashSet<int> second = new HashSet<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+            first.Add(input);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+            second.Add(input);
+        }
+
+        Console.Write(string.Join(" ", (first.Intersect(second))));
+    }
+
+}
+
