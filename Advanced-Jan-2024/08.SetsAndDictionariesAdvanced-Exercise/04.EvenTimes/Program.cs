80970d6b353846763c75045abe0404f897a53ea7
Avimaker <Avimaker@users.noreply.github.com>
2024-01-20 14:29:17 +0200

    EvenTimes Solved

---
+++ Advanced-Jan-2024/08.SetsAndDictionariesAdvanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+3
+2
+-1
+2
+
+5
+1
+2
+3
+1
+5
+*/
+
+namespace _04.EvenTimes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, int> evenNumberList = new Dictionary<int, int>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int counter = 0;
+            int input = int.Parse(Console.ReadLine());
+            if (!evenNumberList.ContainsKey(input))
+            {
+                evenNumberList.Add(input, counter);
+            }
+            evenNumberList[input]++;
+        }
+
+        //foreach (var even in evenNumberList)
+        //{
+        //    if (even.Value % 2== 0)
+        //    {
+        //        Console.WriteLine(even.Key);
+        //    }
+        //}
+
+        Console.WriteLine(evenNumberList.Single(nc => nc.Value % 2 == 0).Key);
+    }
+}
+
