2ea704f0fe76cbdfa1edf4024798ab8fb06609d8
Avimaker <Avimaker@users.noreply.github.com>
2024-01-20 14:07:08 +0200

    PeriodicTable solved

---
+++ Advanced-Jan-2024/08.SetsAndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+4
+Ce O
+Mo O Ce
+Ee
+Mo
+
+3
+Ge Ch O Ne
+Nb Mo Tc
+O Ne
+
+*/
+
+namespace _03.PeriodicTable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        SortedSet<string> elements = new SortedSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            for (int k = 0; k < input.Length; k++)
+            {
+                elements.Add(input[k]);
+            }
+
+        }
+
+        //foreach (var element in elements)
+        //{
+        //    Console.Write($"{element} ");
+        //}
+
+        Console.Write(string.Join(" ", elements));
+
+    }
+}
+
