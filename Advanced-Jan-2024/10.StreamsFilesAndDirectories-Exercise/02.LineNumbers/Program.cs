dac70571e6824c8f119c4a7b92959ee7c8a7ceaf
Avimaker <Avimaker@users.noreply.github.com>
2024-01-22 20:20:44 +0200

    LineNumbers Solved

---
+++ Advanced-Jan-2024/10.StreamsFilesAndDirectories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LineNumbers;
+
+public class LineNumbers
+{
+    static void Main()
+    {
+        string inputFIlePath = @"../../../text.txt";
+        string outputFIlePath = @"../../../output.txt";
+
+        ProcessLines(inputFIlePath, outputFIlePath);
+    }
+
+    public static void ProcessLines(string inputFilePath, string outputFilePath)
+    {
+
+        StringBuilder text = new();
+
+        string[] lines = File.ReadAllLines(inputFilePath);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int lettersCount = lines[i].Count(char.IsLetter);
+            int symbolsCount = lines[i].Count(char.IsPunctuation);
+
+            text.Append($"Line {i + 1}: {lines[i]} ({lettersCount})({symbolsCount})");
+        }
+
+        File.WriteAllText(outputFilePath, text.ToString().TrimEnd());
+    }
+}
