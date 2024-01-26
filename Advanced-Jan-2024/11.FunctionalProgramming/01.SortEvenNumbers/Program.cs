e3d81f41de46d6dc4c40b502011c462ed16ca878
Avimaker <Avimaker@users.noreply.github.com>
2024-01-26 23:43:43 +0200

    ActionPrint solved

---
+++ Advanced-Jan-2024/11.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.SortEvenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ Advanced-Jan-2024/12.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Lucas Noah Tea 
+*/
+
+namespace _01.ActionPrint;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Action<string[]> print = strings =>
+    Console.WriteLine(string.Join(Environment.NewLine, strings));
+
+
+        print(Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+    }
+}
+
