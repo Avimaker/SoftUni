9414a499eeded910672ea009a3c8e35b3919dd63
Avimaker <Avimaker@users.noreply.github.com>
2024-01-27 19:54:57 +0200

    FindEvensOrOdds Solved

---
+++ Advanced-Jan-2024/12.FunctionalProgramming-Exercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+1 10
+odd
+
+20 30
+even
+
+*/
+
+namespace _04.FindEvensOrOdds;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] inputRange = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        List<int> range = new List<int>();
+        range = Enumerable.Range(inputRange[0], (inputRange[1] - inputRange[0] + 1)).ToList();
+
+        string command = Console.ReadLine();
+
+        Predicate<int> result = x => x % 2 == 0;
+
+        if (command == "even")
+        {
+            foreach (var number in range)
+            {
+                if (result(number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+
+        }
+        if (command == "odd")
+        {
+            foreach (var number in range)
+            {
+                if (!result(number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+
+        }
+
+    }
+}
+
