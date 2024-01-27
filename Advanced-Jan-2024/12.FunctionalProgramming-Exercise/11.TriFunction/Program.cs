e8ca2652c75a0c99d38dbeee36454581ef7c1192
Avimaker <Avimaker@users.noreply.github.com>
2024-01-27 23:15:28 +0200

    TriFunction

--- Advanced-Jan-2024/12.FunctionalProgramming-Exercise/11.TriFunction/Program.cs
+++ Advanced-Jan-2024/12.FunctionalProgramming-Exercise/11.TriFunction/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace _11.TriFunction;
+﻿/*
+350
+Rob Mary Paisley Spencer
+ 
+*/
+
+namespace _11.TriFunction;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Func<string, int, bool> checkEqualOrLargerNameSum =
+    //(name, sum) => name.Sum(ch => ch) >= sum;
+    (name, sum) =>
+    {
+        int charsSum = name.Sum(ch => ch);
+
+        return charsSum >= sum;
+    };
+
+
+        Func<string[], int, Func<string, int, bool>, string> getFirstName =
+            //(names, sum, match) => names.FirstOrDefault(name => match(name, sum));
+            (names, sum, match) =>
+            {
+                foreach (var name in names)
+                {
+                    if (match(name, sum))
+                    {
+                        return name;
+                    }
+                }
+
+                return default;
+            };
+
+        int sum = int.Parse(Console.ReadLine());
+
+        string[] names = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string foundName = getFirstName(names, sum, checkEqualOrLargerNameSum);
+
+        Console.WriteLine(foundName);
     }
 }
 
