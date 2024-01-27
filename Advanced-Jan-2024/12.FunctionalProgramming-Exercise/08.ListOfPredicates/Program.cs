436b3cf7b1f8256e22832784a871dcd53163f692
Avimaker <Avimaker@users.noreply.github.com>
2024-01-27 22:46:19 +0200

    PredicateParty

--- Advanced-Jan-2024/12.FunctionalProgramming-Exercise/08.ListOfPredicates/Program.cs
+++ Advanced-Jan-2024/12.FunctionalProgramming-Exercise/08.ListOfPredicates/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace _08.ListOfPredicates;
+﻿/*
+10
+1 1 1 2
+
+100
+2 5 10 20
+ 
+*/
+
+namespace _08.ListOfPredicates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Predicate<int>> predicates = new();
+
+        int endRange = int.Parse(Console.ReadLine());
+
+        HashSet<int> dividers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToHashSet();
+
+        int[] numbers = Enumerable.Range(1, endRange).ToArray();
+
+        foreach (var divider in dividers)
+        {
+            //Predicate<int> checkDivisible = n =>
+            //    n % divider == 0;
+
+            predicates.Add(number => number % divider == 0);
+        }
+
+        foreach (var number in numbers)
+        {
+            bool isDivisible = true;
+
+            foreach (var match in predicates)
+            {
+                if (!match(number))
+                {
+                    isDivisible = false;
+                    break;
+                }
+            }
+
+            if (isDivisible)
+            {
+                Console.Write($"{number} ");
+            }
+        }
     }
 }
 
---
+++ Advanced-Jan-2024/12.FunctionalProgramming-Exercise/09.PredicateParty!/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Peter Misha Stephen
+Remove StartsWith P
+Double Length 5
+Party!
+ 
+*/
+
+namespace _09.PredicateParty_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> people = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+        string command;
+        while ((command = Console.ReadLine()) != "Party!")
+        {
+            string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string action = tokens[0];
+            string filter = tokens[1];
+            string value = tokens[2];
+
+            if (action == "Remove")
+            {
+                people.RemoveAll(GetPredicate(filter, value));//тук от метода взима кейс
+            }
+            else
+            {
+                List<string> peopleToDouble = people.FindAll(GetPredicate(filter, value));//ако има такъв човек го добавя към списък
+
+                foreach (string person in peopleToDouble)
+                {
+                    int index = people.FindIndex(p => p == person);//намира индекса на човекам който ще добавяме
+                    people.Insert(index + 1, person);// добавяме на индекса, който намерихме
+                }
+            }
+        }
+
+        if (people.Any())
+        {
+            Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+        }
+        else
+        {
+            Console.WriteLine("Nobody is going to the party!");
+        }
+
+
+        static Predicate<string> GetPredicate(string filter, string value)
+        {
+            switch (filter)
+            {
+                case "StartsWith":
+                    return p => p.StartsWith(value);
+                case "EndsWith":
+                    return p => p.EndsWith(value);
+                case "Length":
+                    return p => p.Length == int.Parse(value);
+                default:
+                    return default;
+            }
+        }
+    }
+}
+
