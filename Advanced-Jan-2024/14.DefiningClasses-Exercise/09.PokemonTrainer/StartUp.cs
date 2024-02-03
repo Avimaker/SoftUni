1b6c0e4b0ce4b38249312f1703df73a286eeb87c
Avimaker <Avimaker@users.noreply.github.com>
2024-02-03 19:54:29 +0200

    .

---
+++ Advanced-Jan-2024/14.DefiningClasses-Exercise/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,60 @@
+﻿/*
+Peter Charizard Fire 100
+George Squirtle Water 38
+Peter Pikachu Electricity 10
+Tournament
+Fire
+Electricity
+End
+
+*/
+
+namespace PokemonTrainer;
+class StartUp
+{
+    static void Main()
+    {
+
+        HashSet<Trainer> trainers = new();
+
+        string command = default;
+        while ((command = Console.ReadLine()) != "Tournament")
+        {
+            string[] arguments = command.Split();
+            string trainerName = arguments[0];
+            string pokemonName = arguments[1];
+            string pokemonElement = arguments[2];
+            int pokemonHealth = int.Parse(arguments[3]);
+
+            Trainer trainer = trainers.SingleOrDefault(n => n.Name == trainerName);//проверява дали името го има в списъка, ако не връща null
+            Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);//създавам си покемона
+
+            if (trainer == null)
+            {
+                trainer = new(trainerName);//извикваме конструктора на класа трейнър, там създаваме име и инстанция на списък
+                trainer.Pokemons.Add(pokemon);//добавяме инфото за покемона в този списък
+                trainers.Add(trainer);//добавяме временния обект в крайния списък
+            }
+            else
+            {
+                trainer.Pokemons.Add(pokemon);//вече имаме име и само добавяме към списъка му покемони
+            }
+        }
+        while ((command = Console.ReadLine()) != "End")
+        {
+            foreach (var trainer in trainers)//за всеки от списъка правим проверка за покемоните
+            {
+                trainer.CheckPokemon(command);
+            }
+        }
+
+        foreach (var trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+        }
+
+    }
+
+
+}
+
---
+++ Advanced-Jan-2024/14.DefiningClasses-Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace PokemonTrainer
+{
+	public class Trainer
+	{
+		private string name;
+		private int numberOfBadges;
+		private List<Pokemon> pokemons;//когато имаме лист задължително правим инстнция в конструктора
+
+        public Trainer(string name)
+        {
+            Name = name; //добавяме име на листа
+            Pokemons = new(); //правим инстанция на списъка с дефолтна стойност
+        }
+
+        public string Name
+		{
+			get { return this.name; }
+			set { this.name = value; }
+		}
+
+		public int NumberOfBadges
+        {
+			get { return this.numberOfBadges; }
+			set { this.numberOfBadges = value; }
+		}
+
+		public List<Pokemon> Pokemons
+        {
+			get { return pokemons; }
+			set { this.pokemons = value; }
+		}
+
+		public void CheckPokemon(string command)
+		{
+			if (Pokemons.Any(p => p.Element == command))//проверяваме дали елемента съшествува в списъка с покемони
+			{
+				numberOfBadges++; //ако да добавяме една значка
+			}
+			else //ако не ..
+			{
+				for (int i = 0; i < Pokemons.Count; i++) //въртим по списъка с покемони и намаляваме здравето на всеки с 10
+				{
+					Pokemon currentPokemon = Pokemons[i];
+					currentPokemon.Health -= 10;
+
+					if (currentPokemon.Health <= 0) //ако здравето на временния покемон падне под нула или е раяно на 0 го махаме от обшия списък
+					{
+						Pokemons.Remove(currentPokemon);
+					}
+				}
+			}
+
+		}
+	}
+}
+
