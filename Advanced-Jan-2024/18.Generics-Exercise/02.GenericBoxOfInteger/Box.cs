0e8c18652611564ce20e49ff54b78206b5cd8664
Avimaker <Avimaker@users.noreply.github.com>
2024-02-05 19:41:17 +0200

    GenericBoxOfInteger solved

---
+++ Advanced-Jan-2024/18.Generics-Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace GenericBoxOfInteger;
+
+public class Box<T>
+{
+    //private List<T> items = new List<T>();//short way
+    private List<T> items;
+
+    public Box()
+    {
+        items = new List<T>();//if i use short way - this line have to be deleted
+    }
+
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+
+        foreach (var item in items)
+        {
+            sb.AppendLine($"{typeof(T)}: {item.ToString()}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+}
+
+
--- Advanced-Jan-2024/18.Generics-Exercise/02.GenericBoxOfInteger/StartUp.cs
+++ Advanced-Jan-2024/18.Generics-Exercise/02.GenericBoxOfInteger/StartUp.cs
@@ -1,9 +1,30 @@
-﻿namespace GenericBoxOfInteger;
-class Program
+﻿/*
+1
+1001
+
+3
+7
+123
+42
+*/
+
+
+namespace GenericBoxOfInteger;
+class StartUp
 {
-    static void Main()
+    static void Main(string[] args)
     {
-       
+
+        Box<int> box = new();
+
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            int item = int.Parse(Console.ReadLine());
+            box.Add(item);
+        }
+
+        Console.WriteLine(box.ToString());
     }
 }
-
