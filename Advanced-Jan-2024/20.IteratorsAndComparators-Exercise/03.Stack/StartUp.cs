8812e7efdeb4b076427fc09f801335190d69146d
Avimaker <Avimaker@users.noreply.github.com>
2024-02-10 00:22:02 +0200

    finished problems

---
+++ Advanced-Jan-2024/20.IteratorsAndComparators-Exercise/03.Stack/StartUp.cs
@@ -0,0 +1,74 @@
+﻿/*
+Push 1, 2, 3, 4
+Pop
+Pop
+END
+
+Push 1, 2, 3, 4 
+Pop
+Push 1
+END
+
+Push 1, 2, 3, 4 
+Pop
+Pop
+Pop
+Pop
+Pop
+END
+
+*/
+
+namespace Stack;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        CustomStack<int> stack = new();
+
+        string command = string.Empty;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            string[] tokens = command
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string action = tokens[0];
+
+            if (action == "Push")
+            {
+                int[] itemsToPush = tokens
+                    .Skip(1)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                foreach (var item in itemsToPush)
+                {
+                    stack.Push(item);
+                }
+            }
+            else
+            {
+                try
+                {
+                    stack.Pop();
+                }
+                catch (InvalidOperationException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
---
+++ Advanced-Jan-2024/20.IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+1, 2, 3, 4, 5, 6, 7, 8
+1, 2, 3, 4, 5
+13, 23, 1, -8, 4, 9
+ 
+*/
+
+namespace Froggy;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> stones = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        Lake lake = new(stones);
+
+        Console.WriteLine(string.Join(", ", lake));
+    }
+}
+
