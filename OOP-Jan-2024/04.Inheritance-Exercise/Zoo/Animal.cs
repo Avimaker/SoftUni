62c7d2c5718a75b340f6813fff49d2d5f4583574
Avimaker <Avimaker@users.noreply.github.com>
2024-02-23 14:41:34 +0200

    Zoo solved

---
+++ OOP-Jan-2024/04.Inheritance-Exercise/Zoo/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Zoo
+{
+    public abstract class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+}
+
